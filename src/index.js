0ab8da12dcffe980bf8d6c616df576f41216b29f
Carmen Morata <cmoratagarcia@gmail.com>
2024-04-08 11:40:51 -0700

    Add event listeners to nav buttons on index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@ import homepage from "./homepage.js";
 import menu from "./menu.js";
 import contact from "./contact.js";
 
+const homeBtn = document.getElementById("home-btn");
+homeBtn.addEventListener("click", () => {
+  handleClick(homepage);
+});
+const menuBtn = document.getElementById("menu-btn");
+menuBtn.addEventListener("click", () => {
+  handleClick(menu);
+});
+const contactBtn = document.getElementById("contact-btn");
+contactBtn.addEventListener("click", () => {
+  handleClick(contact);
+});
 const content = document.getElementById("content");
 content.appendChild(homepage());
+
+function handleClick(selectedPage) {
+  content.innerHTML = "";
+  content.appendChild(selectedPage());
+}
